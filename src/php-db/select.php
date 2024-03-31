31e48ba8afd8d003511aef621a291e632cc78bc6
ueni-tech <ueni.tech@gmail.com>
2024-03-31 12:09:40 +0900

    #1 add: insert, select

---
+++ src/php-db/select.php
@@ -0,0 +1,38 @@
+<?php
+  $dsn = 'mysql:dbname=php_db;host=mysql;charset=utf8mb4';
+  $user = 'root';
+  $password = 'password';
+
+  try {
+    $pdo = new PDO($dsn, $user, $password);
+    $sql = 'SELECT id, name FROM users';
+
+    $stmt = $pdo->query($sql);
+    $results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+  } catch(PDOException $e){
+    exit($e->getMessage());
+  }
+  ?>
+
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Document</title>
+  <link rel="stylesheet" href="./css/style.css">
+</head>
+<body>
+  <table>
+    <tr>
+      <th>ID</th>
+      <th>氏名</th>
+    </tr>
+    <?php
+    foreach($results as $result) {
+      echo "<tr><td>{$result['id']}</td><td>{$result['name']}</td></tr>";
+    }
+    ?>
+  </table>
+</body>
+</html>
